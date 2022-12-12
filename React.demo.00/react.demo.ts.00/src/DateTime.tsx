5045326e2077359ee1715f50c71fbd785a24408a
Patrick Boake <patrick@xrpl-labs.com>
2022-12-12 14:23:33 -0500

    maintenance

---
+++ React.demo.00/react.demo.ts.00/src/DateTime.tsx
@@ -0,0 +1,10 @@
+export{}
+function dateTest(date:Date):void{
+    var hours=date.getHours();
+    var minutes=date.getMinutes();
+    var seconds=date.getSeconds();
+    console.log("Current Time: "+hours+":"+minutes+":"+seconds); 
+  }
+  var date=new Date();
+  dateTest(date);
+  
